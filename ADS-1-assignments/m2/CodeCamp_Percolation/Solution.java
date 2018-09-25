5346c95008eac3291cc9a851fcc287766a2dbc34
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-09-26 01:59:31 +0530

    "check_style_score -> 18 of 24 passed. style score: 0/1 "

--- ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1-assignments/m2/CodeCamp_Percolation/Solution.java
@@ -24,60 +24,47 @@ class Percolation {
 	Percolation(int n) {
 		size = n;
 		sites = new int[n*n + 2];
-		// System.out.println(Arrays.toString(sites));
 		id = new int[n*n + 2];
 		int count = 1;
 		for(int i = 0; i < size * size + 2; i++) {
 			id[i] = count++;
 		}
-		// System.out.println(Arrays.toString(id));
-		// for(int i = 0; i < n-1; i++) {
-		// 	union(i, id[(n*n)-1]);
-		// }
-		// for(int i = (n*n - size) - 1; i < n * n; j++) {
-		// 	union(i, id[n*n]);
-		// }
 	}
 	public void union(int p, int q) {
-		// System.out.println(p + ":" +q);
+		int pid = id[p];
+		int qid = id[q];
 		if(isOpen(q)) {
-			id[p] = id[q];
+			for(int i = 0; i < size * size; i++) {
+				if(id[i] == qid) {
+					id[i] = pid;
+				}
+			}
 		}
 	}
-
-	public int find(int i) {
-		int key = id[i];
-		// System.out.println("my key "+key);
-		// System.out.println("my eye "+i);
-		// System.out.println("my id[i] "+id[i]);
-		while(id[i] == key) {
-			i = key;
-		}
-		return i;
+	public Boolean connected(int i, int j) {
+		return(id[i] == id[j]);
 	}
 
 	public Boolean isOpen(int index) {
 		// System.out.println(index);
-		return id[index] == 1;
+		// System.out.println(sites[index] == 1);
+		return sites[index] == 1;
 	}
 
 	public void  open(int row, int col) {
 		int index = ((row-1) * size + col) - 1;
-		// System.out.println("index:"+index);
-		//For first row
 		sites[index] = 1;
 		sites[size*size+1]=1;
 		sites[size*size]=1;
-		// System.out.println(Arrays.toString(sites));
+		//For first row
 		if(index > 0 && index < size-1) {
-			// System.out.println("row one");
 			union(index, index - 1);
 			union(index, index+1);
 			union(index, index+size);
 		}
 		//For first column
 		else if((index % size == 0) &&(index>0)&& (index < size*size -size)) {
-			// System.out.println("first coll");
+			// System.out.println("Hello");
 			union(index, index+1);
 			union(index, index + size);
 			union(index, index-size);
@@ -85,40 +72,33 @@ class Percolation {
 		//For Last column
 		else if(index % size == size - 1 && (index>size)&& (index < size*size - 1) ) {
 			union(index, index -1);
-			// System.out.println("laaastttttttt col");
 			union(index, index + size);
 			union(index, index - size);	
 		}
 		//For Last row
-		// System.out.printlnSystem.out.println("one");
-		// ("one");
 		else if(index > (size * size - size)  && index < (size * size) - 1) {
-			// System.out.println("last row");
 			union(index, index - size);
 			union(index, index - 1);
 			union(index, index + 1);
 		}
 		//For UpperLeft corner
 		else if(index == 0) {
-			// System.out.println("UpperLeft");
 			union(index, index + 1);
 			union(index, index + size);
 		}
 		//For UpperRight corner
 		else if(index == size - 1) {
-			// System.out.println("UpperRight");
 			union(index, index - 1);
 			union(index, index + size);
 		}
 		//For BottomLeft Corner
 		else if(index == (size * (size - 1))) {
+			// System.out.println("Hi");
 			union(index, index + 1);
-// System.out.println("BottomLeft");
 			union(index, index - size);
 		}
 		//For BottomRight Corner
 		else if(index == (size*size)-1) {
-// System.out.println("BottomRight");
 			union(index, index-1);
 			union(index, index-size);
 		}
@@ -126,36 +106,25 @@ class Percolation {
 			union(index, index - 1);
 			union(index, index + 1);
 			union(index, index - size);
-			// System.out.println("others");
 
 			union(index, index + size);
 		}
 		count++;
 		// System.out.println(Arrays.toString(id));
+		// System.out.println(Arrays.toString(sites));
 	}
 
 	public Boolean percolates() {
-		// System.out.println("nenu ochesa");
-		// System.out.println("my size ==="+size);
 		for(int i = 0; i < size; i++) {
-			// System.out.println("\ni robo = "+ i);
-
-			union(id[(size*size)],i);
-			// System.out.println("\nevry loop "+Arrays.toString(id));
-		// System.out.println("every sites"+Arrays.toString(sites));
-
+			union(size * size, i);
+			// System.out.println(Arrays.toString(id));
+			for(int j = ((size*size) - size) - 1; j < size*size; j++) {
+				if(isOpen(j)) {
+					return true;
+				}
+			}
 		}
-		// System.out.println("\npercilste "+Arrays.toString(id));
-		// System.out.println("percilste sites"+Arrays.toString(sites));
-		for(int i = (size*size - size); i < size * size; i++) {
-			// System.out.println("my bobo = " + i);
-			union(id[size*size],i);
-		}
-		// System.out.println("wewr "+Arrays.toString(id));
-		// System.out.println("my s *s "+size*size);
-		// System.out.println("percilste sites"+Arrays.toString(sites));
-		// System.out.println("sites size sites"+sites.length);
-		return find(size*size) != find(size*size+1);
+		return false;
 	}
 }
 
@@ -169,9 +138,6 @@ public class Solution {
 			String[] val = input.split(" ");
 			p.open(Integer.parseInt(val[0]), Integer.parseInt(val[1]));
 		}
-		Percolation pp = new Percolation();
-		// System.out.println("Hi");
 		System.out.println(p.percolates());
-		// System.out.println("nenu ranu");
 	}
 }
