8e64a8994fb04f0f5219fd2326ea5d76e613b439
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-09-27 17:37:09 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,96 +1,119 @@
 import java.util.Scanner;
+/**
+ * Class for stack.
+ */
 class Stack {
-	
-	String[] data = new String[2000];
-	int top = -1;
-	public int push(char input) {
-		String val = Character.toString(input);
-		if(top == -1) {
-			if(val.equals("(") || val.equals("[") || val.equals("{")) {
-				top++;
-				data[top] = val;
-				return 1;
-			} 
-			else {
-				return 0;
-			}
-		} 
-		switch(val) {
-			case "(":
-			if(data[top].equals("(") || data[top].equals("[") || data[top].equals("{") ) {
-				top++;
-				data[top] = val;
-				return 1;
-			}
-			return 0;
-			case ")":
-			if(data[top].equals("(")) {
-				pop();
-				return 1;
-			}
-			return 0;
-			case "[":
-			if(data[top].equals("(") || data[top].equals("[") || data[top].equals("{")) {
-				top++;
-				data[top] = val;
-				return 1;
-			}
-			return 0;
-			case "]":
-			if(data[top].equals("[")) {
-				pop();
-				return 1;
-			}
-			return 0;
-			case "{":
-			if(data[top].equals("(") || data[top].equals("[") || data[top].equals("{")) {
-				top++;
-				data[top] = val;
-				return 1;
-			}
-			return 0;
-			case "}":
-			if(data[top].equals("{")) {
-				pop();
-				return 1;
-			}
-			return 0;
-		}
-		return 0;
-	}
+    private String[] data = new String[2000];
+    private int top = -1;
+    /**
+     * pushes the element into the stack for valid inputs.
+     *
+     * @param      input  The input
+     *
+     * @return     1 if input is valid, else false
+     */
+    public int push(char input) {
+        String val = Character.toString(input);
+        if(top == -1) {
+            if(val.equals("(") || val.equals("[") || val.equals("{")) {
+                top++;
+                data[top] = val;
+                return 1;
+            } 
+            else {
+                return 0;
+            }
+        } 
+        switch(val) {
+            case "(":
+            if(data[top].equals("(") || data[top].equals("[") || data[top].equals("{") ) {
+                top++;
+                data[top] = val;
+                return 1;
+            }
+            return 0;
+            case ")":
+            if(data[top].equals("(")) {
+                pop();
+                return 1;
+            }
+            return 0;
+            case "[":
+            if(data[top].equals("(") || data[top].equals("[") || data[top].equals("{")) {
+                top++;
+                data[top] = val;
+                return 1;
+            }
+            return 0;
+            case "]":
+            if(data[top].equals("[")) {
+                pop();
+                return 1;
+            }
+            return 0;
+            case "{":
+            if(data[top].equals("(") || data[top].equals("[") || data[top].equals("{")) {
+                top++;
+                data[top] = val;
+                return 1;
+            }
+            return 0;
+            case "}":
+            if(data[top].equals("{")) {
+                pop();
+                return 1;
+            }
+            return 0;
+        }
+        return 0;
+    }
+    /**
+     * Pops the element
+     */
+    public void pop() {
+        top--;
+    }
+    /**
+     * Gets the top.
+     *
+     * @return     The top.
+     */
+    public int getTop() {
+        return top;
+    }
 
-	public void pop() {
-		top--;
-	}
+    public void setTop(int i) {
+        top = i;
+    }
 }
 public class Solution {
-	public static void main(String[] args) {
-		Stack st = new Stack();
-		Scanner s = new Scanner(System.in);
-		int lines = s.nextInt();
-		s.nextLine();
-		int i = 0;
-		while(i < lines) {
-			String inputs = "";
-			inputs = s.nextLine();
-			int j = 0, f = 0;
-			st.top = -1;
-			for(j = 0; j < inputs.length(); j++) {
-				f = st.push(inputs.charAt(j));
-				if(f == 0) {
-					System.out.println("NO");
-					break;
-				}
-			}
-			if(f!=0) {
-				if(j == inputs.length() && st.top == -1) {
-					System.out.println("YES");
-				}
-				else {
-					System.out.println("NO");	
-				}
-			}
-			i++;
-		}
-	}
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        Scanner s = new Scanner(System.in);
+        int lines = s.nextInt();
+        s.nextLine();
+        int i = 0;
+        while(i < lines) {
+            String inputs = "";
+            inputs = s.nextLine();
+            int j = 0, f = 0;
+            st.setTop(-1);
+            for(j = 0; j < inputs.length(); j++) {
+                f = st.push(inputs.charAt(j));
+                if(f == 0) {
+                    System.out.println("NO");
+                    break;
+                }
+            }
+            if(f!=0) {
+                if(j == inputs.length() && st.getTop() == -1) {
+                    System.out.println("YES");
+                }
+                else {
+                    System.out.println("NO");   
+                }
+            }
+            i++;
+        }
+    }
 }
