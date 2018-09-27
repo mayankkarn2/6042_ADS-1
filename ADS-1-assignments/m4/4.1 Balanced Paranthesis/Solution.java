58d4aba285a3443f0b654345f3d184cd8f052ebe
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-09-27 17:21:01 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -81,7 +81,7 @@ public class Solution {
 					break;
 				}
 			}
-			if(j == inputs.length()-1 && st.top == 0) {
+			if(j == inputs.length()-1 && st.top == -1) {
 				System.out.println("YES");
 			}
 			else {
