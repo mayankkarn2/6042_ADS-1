ed035963cbf6ad2e9f073f17b8ea062d69dbb78a
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-09-29 12:57:57 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

--- ADS-1-assignments/m6/Assignment-1/Solution.java
+++ ADS-1-assignments/m6/Assignment-1/Solution.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 /**
  * Class for add large numbers.
  */
-class AddLargeNumbers {
+final class AddLargeNumbers {
     /**
      * Constructs the object.
      */
@@ -60,11 +60,10 @@ class AddLargeNumbers {
             for (int i = 0; i < difference; i++) {
                 two.push('0');
             }
-            for (int i = 0 ; i < second.length(); i++) {
+            for (int i = 0; i < second.length(); i++) {
                 two.push(second.charAt(i));
             }
-        }
-        else if (first.length() - second.length() == 0) {
+        } else if (first.length() - second.length() == 0) {
             for (int i = 0; i < first.length(); i++) {
                 one.push(first.charAt(i));
                 // System.out.println("hi");
@@ -72,12 +71,11 @@ class AddLargeNumbers {
             for (int i = 0; i < second.length(); i++) {
                 two.push(second.charAt(i));
             }
-        }
-        else {
+        } else {
             for (int i = 0; i < difference; i++) {
                 one.push('0');
             }
-            for (int i = 0 ; i < first.length(); i++) {
+            for (int i = 0; i < first.length(); i++) {
                 one.push(first.charAt(i));
             }
             for (int i = 0; i < second.length(); i++) {
@@ -98,14 +96,14 @@ class AddLargeNumbers {
             if (one.size() == 0) {
                 String s = Integer.toString(sum);
                 // System.out.println(s);
-                for(int i = s.length()-1; i >= 0; i--) {
+                for (int i = s.length() - 1; i >= 0; i--) {
                     res.push(s.charAt(i));
                 }
-            }
-            else {
-                res.push(Character.forDigit(sum % 10, 10));
+            } else {
+                final int ten = 10;
+                res.push(Character.forDigit(sum % ten, ten));
                 // System.out.println(Character.forDigit(sum % 10, 10));
-                carry.push(Character.forDigit(sum / 10, 10));
+                carry.push(Character.forDigit(sum / ten, ten));
                 // System.out.println(Character.forDigit(sum / 10, 10));
                 // System.out.println("--------------------------------------");
             }
@@ -121,7 +119,7 @@ class AddLargeNumbers {
 /**
  * Class for solution.
  */
-public class Solution {
+final public class Solution {
     /**
      * Constructs the object.
      */
@@ -151,6 +149,8 @@ public class Solution {
                 qDigits = AddLargeNumbers.numberToDigits(q);
                 LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
                 System.out.println(AddLargeNumbers.digitsToNumber(result));
+                break;
+            default:
                 break;
         }
     }
