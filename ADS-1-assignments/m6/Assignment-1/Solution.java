1983581b8f37bdaa714be187728c10e170db7107
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-09-29 11:58:29 +0530

    "check_style_score -> 10 of 12 passed. style score: 0/1 "

--- ADS-1-assignments/m6/Assignment-1/Solution.java
+++ ADS-1-assignments/m6/Assignment-1/Solution.java
@@ -22,12 +22,36 @@ class LinkedList {
         }
         return numer;
     }
+}
 
+class Stack {
+    
+    char[] data = new char[500];
+    int top = -1;
+    public void push(char input) {
+        top++;
+        data[top] = input;
+    }
+
+    public char pop() {
+        char r = data[top];
+        top--;
+        return r;
+    }
+    public int size() {
+        return top+1;
+    }
+    public String print() {
+        String s = "";
+        for(int i = 0; i <= top; i++) {
+            s = s + (data[i]);
+        }
+        return s;
+    }
 }
 class AddLargeNumbers {
     
     public static LinkedList numberToDigits(String number) {
-        // System.out.println(number);
         LinkedList num = new LinkedList();
         for(int i = 0; i < number.length(); i++) {
             num.addAtHead(number.charAt(i));
@@ -41,7 +65,57 @@ class AddLargeNumbers {
     }
 
     public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-        return null;
+        String first = digitsToNumber(list1);
+        String second = digitsToNumber(list2);
+        Stack one = new Stack();
+        Stack two = new Stack();
+        Stack carry = new Stack();
+        int difference = first.length() - second.length();
+        if(difference < 0) {
+            difference = -1 * (difference);
+        }
+
+        if (first.length() - second.length() > 0) {
+            for(int i = 0; i < first.length(); i++) {
+                one.push(first.charAt(i));
+            }
+            for(int i = 0; i < difference; i++) {
+                two.push('0');
+            }
+            for(int i = 0 ; i < second.length(); i++) {
+                two.push(second.charAt(i));
+            }
+        }
+        else {
+            for(int i = 0; i < difference; i++) {
+                one.push('0');
+            }
+            for(int i = 0 ; i < first.length(); i++) {
+                one.push(first.charAt(i));
+            }
+            for(int i = 0; i < second.length(); i++) {
+                two.push(second.charAt(i));
+            }
+        }
+        one.print();
+        two.print();
+        carry.push('0');
+        Stack res = new Stack();
+        while(one.top != -1) {
+            int n1 = Integer.parseInt(String.valueOf(one.pop()));
+            int n2 = Integer.parseInt(String.valueOf(two.pop()));
+            int sum = n1 + n2 + Integer.parseInt(String.valueOf(carry.pop()));
+            // if(Integer.parseInt(String.valueOf(carry.pop())) > 0) {
+            //     sum = n1 + n2 + Integer.parseInt(String.valueOf(carry.pop()));
+            //     char c = carry.pop();
+            // }
+            res.push(Character.forDigit(sum % 10, 10));
+            carry.push(Character.forDigit(sum / 10, 10));
+        }
+        StringBuilder sbs = new StringBuilder(res.print());
+        String result = sbs.reverse().toString();
+        LinkedList ret = AddLargeNumbers.numberToDigits(result);
+        return ret;
     }
 }
 
