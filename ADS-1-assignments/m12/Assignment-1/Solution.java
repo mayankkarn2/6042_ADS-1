24b9ed9b6738ed296499dd667dc1f7446d4f0121
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-10-06 12:46:57 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m12/Assignment-1/Solution.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int lines = s.nextInt();
+		int vacancies = s.nextInt();
+		int unreserved = s.nextInt();
+		int bcVac = s.nextInt();
+		int scVac = s.nextInt();
+		int stVac = s.nextInt();
+		int i = 0;
+		s.nextLine();
+		Storage store = new Storage();
+		while(i < lines) {
+			String data = s.nextLine();
+			String[] datas = data.split(",");
+			// System.out.println(datas[]);
+			store.addStudent(new Student(datas[0],datas[1],Integer.parseInt(datas[2]),Integer.parseInt(datas[3]),Integer.parseInt(datas[4]), datas[6]));
+			i++;
+		}
+		Student[] merit = store.meritOrder();
+		Student[] result = new Student[vacancies];
+		store.printStudents();
+		int k = 0;
+		for(i = 0; i < unreserved; i++) {
+			// System.out.print(merit[i].getName()+",");
+			// System.out.print(merit[i].getTotal()+",");
+			// System.out.println(merit[i].getCat());
+			result[k] = merit[i];
+			merit[i].setInc("INC");
+			k++;
+		}
+		int count = 0;
+		for(i = 0; i < merit.length; i++) {
+			if(count == bcVac) break;
+			// System.out.println("hi");
+			if(merit[i].getCat().equals("BC") && merit[i].getInc().equals("n")) {
+				// System.out.print(merit[i].getName()+",");
+				// System.out.print(merit[i].getTotal()+",");
+				// System.out.println(merit[i].getCat());
+				result[k] = merit[i];
+				count++;
+				k++;
+			}
+		}
+		count = 0;
+		for(i = 0; i < merit.length; i++) {
+			if(count == scVac) break;
+			if(merit[i].getCat().equals("SC") && merit[i].getInc().equals("n")) {
+				// System.out.print(merit[i].getName()+",");
+				// System.out.print(merit[i].getTotal()+",");
+				// System.out.println(merit[i].getCat());
+				result[k] = merit[i];
+				count++;
+				k++;
+			}
+		}
+		count = 0;
+		for(i = 0; i < merit.length; i++) {
+			if(count == stVac) break;
+			if(merit[i].getCat().equals("ST") && merit[i].getInc().equals("n")) {
+				// System.out.print(merit[i].getName()+",");
+				// System.out.print(merit[i].getTotal()+",");
+				// System.out.println(merit[i].getCat());
+				result[k] = merit[i];
+				count++;
+				k++;
+			}
+		}
+		// System.out.println(k);
+		for(i = 0; i < k; i++) {
+			for(int j = i+1; j < k; j++) {
+				if(result[j].getTotal() > result[i].getTotal()) {
+					Student temp = result[i];
+					result[i] = result[j];
+					result[j] = temp;
+				}
+			}
+		}
+		for(i = 0; i < k; i++) {
+			System.out.print(result[i].getName()+",");
+			System.out.print(result[i].getTotal()+",");
+			System.out.println(result[i].getCat());
+		}
+	}
+}
---
+++ ADS-1-assignments/m12/Assignment-1/Storage.java
@@ -0,0 +1,94 @@
+class Storage {
+	private Student[] students;
+	private int size;
+	private int[] totals;
+
+	Storage() {
+		students = new Student[50];
+		size = 0;
+		totals = new int[50];
+	}
+
+	public void addStudent(Student s) {
+		students[size] = s;
+		totals[size] = s.getTotal();
+		size++;
+	}
+
+	public Student[] meritOrder() {
+		//Sort by Total
+		for(int i = 0; i < size; i++) {
+			for(int j = i+1; j < size; j++) {
+				if(students[j].getTotal() > students[i].getTotal()) {
+					Student temp = students[i];
+					students[i] = students[j];
+					students[j] = temp;
+				}
+			}
+		}
+		//Sort by Subject 3
+		int i = 0;
+		while(i < size-1) {
+			int j = i+1;
+			while(students[i].getTotal() == students[j].getTotal()) {
+				j++;
+			}
+			for(int k = i; k < j; k++) {
+				for(int l = i+1; l < j; l++) {
+					if(students[l].getSub3() > students[k].getSub3()) {
+						Student temp = students[k];
+						students[k] = students[l];
+						students[l] = temp;
+					}
+				}
+			}
+			i++;
+		}
+		//Sort by subject 2
+		i = 0;
+		while(i < size-1) {
+			int j = i+1;
+			while(students[i].getTotal() == students[j].getTotal()) {
+				j++;
+			}
+			for(int k = i; k < j; k++) {
+				for(int l = i+1; l < j; l++) {
+					if(students[l].getSub2() > students[k].getSub2()) {
+						Student temp = students[k];
+						students[k] = students[l];
+						students[l] = temp;
+					}
+				}
+			}
+			i++;
+		}
+		return students;
+		//Sort by DOB
+		// i = 0;
+		// while(i < size-1) {
+		// 	j = i+1;
+		// 	while(students[i].getTotal() == students[j].getTotal()) {
+		// 		j++;
+		// 	}
+		// 	for(int k = i; k < j; k++) {
+		// 		for(int l = i+1; l < j; l++) {
+		// 			if(students[l].getSub2() > students[k].getSub2()) {
+		// 				Student temp = students[k];
+		// 				students[k] = students[l];
+		// 				students[l] = temp;
+		// 			}
+		// 		}
+		// 	}
+		// 	i++;
+		// }
+	}
+
+	public void printStudents() {
+		for(int i = 0; i < size; i++) {
+			System.out.print(students[i].getName()+",");
+			System.out.print(students[i].getTotal()+",");
+			System.out.println(students[i].getCat());
+		}
+		System.out.println();
+	}
+}
