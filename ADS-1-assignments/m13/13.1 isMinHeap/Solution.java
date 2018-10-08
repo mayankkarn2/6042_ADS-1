dc101a0a6694ca4fcae5d036a8051dacc61ec421
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-10-08 21:37:08 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS-1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
+import java.util.Arrays;
 class MinHeap<E> {
 	Comparable[] data;
 	int size;
 	MinHeap(Comparable[] values) {
 		data = values;
 		size = 0;
+		// System.out.println(Arrays.toString(data));
 	}
 	public Boolean isHeap() {
-		for(int i = 0; i < data.length - 2; i++) {
+		// System.out.println(data.length);
+		for(int i = 0; i < (data.length-1)/2 && (2*i+2 <= data.length-1); i++) {
+			// System.out.println("---------");
 			// System.out.println(data[i]);
-			// System.out.println(data[i/2]);
-			// System.out.println(data[i].compareTo(data[i/2]));
+			// System.out.println(data[2*i+1]);
+			// System.out.println(data[2*i+2]);
+			// System.out.println(data[i].compareTo(data[2*i+2]));
 			if(data[i].compareTo(data[2*i+1]) > 0 || data[i].compareTo(data[2*i+2]) > 0) {
 				return false;
 			}
@@ -26,12 +31,12 @@ class Solution {
 		int lines = sc.nextInt();
 		int i = 0;
 		sc.nextLine();
-		String data = sc.nextLine();
-		String[] datas = data.split(",");
 		switch (datatype) {
 			case "Double":
 				while(i < lines) {
 					int j = 0;
+					String data = sc.nextLine();
+					String[] datas = data.split(",");
 					Double[] d = new Double[datas.length];
 					for(String st : datas) {
 						d[j] = Double.parseDouble(st);
@@ -44,6 +49,8 @@ class Solution {
 			case "Integer":
 				while(i < lines) {
 					int j = 0;
+					String data = sc.nextLine();
+					String[] datas = data.split(",");
 					Integer[] d = new Integer[datas.length];
 					for(String st : datas) {
 						d[j] = Integer.parseInt(st);
@@ -56,6 +63,8 @@ class Solution {
 			case "Float":
 				while(i < lines) {
 					int j = 0;
+					String data = sc.nextLine();
+					String[] datas = data.split(",");
 					Float[] d = new Float[datas.length];
 					for(String st : datas) {
 						d[j] = Float.parseFloat(st);
@@ -68,6 +77,8 @@ class Solution {
 			case "String":
 				while(i < lines) {
 					int j = 0;
+					String data = sc.nextLine();
+					String[] datas = data.split(",");
 					MinHeap<String> mh = new MinHeap(datas);
 					System.out.println(mh.isHeap());
 					i++;
