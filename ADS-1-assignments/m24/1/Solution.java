4e2f7248fcbb549fdaa33d4117623473018eaba8
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-10-27 11:45:16 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m24/1/Solution.java
+++ ADS-1-assignments/m24/1/Solution.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.HashMap; 
+import java.util.HashMap;
 /**
  * Class for student.
  */
@@ -73,16 +73,16 @@ final class Solution {
         int m = sc.nextInt();
         HashMap<String, Student> hm = new HashMap<String, Student>();
         sc.nextLine();
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             String[] inputs = sc.nextLine().split(",");
-            hm.put(inputs[0], new Student(inputs[0],inputs[1],
+            hm.put(inputs[0], new Student(inputs[0], inputs[1],
                 Double.valueOf(inputs[2])));
         }
         int n = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < n; i++) {
             String[] inputs = sc.nextLine().split(" ");
-            switch(inputs[2]) {
+            switch (inputs[2]) {
                 case "1":
                 Student s = (hm.get(inputs[1]));
                 if (s == null) {
@@ -94,8 +94,8 @@ final class Solution {
                 case "2":
                 Student st = hm.get(inputs[1]);
                 if (st == null) {
-                    System.out.println
-                    ("Student doesn't exists...");
+                    System.out.println(
+                        "Student doesn't exists...");
                     break;
                 }
                 System.out.println(st.getMarks());
