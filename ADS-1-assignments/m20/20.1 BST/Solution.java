110f2c19e7b8ec4c3627e02ea96864fa68f65d29
mayankkarn2 <mayankkarn2@msitprogram.net>
2018-10-29 14:26:23 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m20/20.1 BST/Solution.java
+++ ADS-1-assignments/m20/20.1 BST/Solution.java
@@ -1,49 +1,105 @@
 import java.util.Scanner;
-
+/**.
+ * Class for book.
+ */
 class Book implements Comparable<Book> {
-
+    /**.
+     * this variable is the name of book
+     */
     private String name;
- 
+    /**.
+     * this variable represents the author name.
+     */
     private String author;
-
+    /**.
+     * the price of the book
+     */
     private double price;
-
+    /**.
+     * Constructs the object.
+     *
+     * @param      bookName    The book name
+     * @param      authorName  The author name
+     * @param      cost        The cost
+     */
     Book(final String bookName, final String authorName,
          final double cost) {
         this.name = bookName;
         this.author = authorName;
         this.price = cost;
     }
-
+    /**
+     * Gets the name.
+     *
+     * @return     The name of book.
+     */
     public String getName() {
         return this.name;
     }
-
+    /**.
+     * This method is to compare both book names
+     * and return its value.
+     * Time Complexity is O(1)
+     *
+     * @param      obj   The object
+     *
+     * @return     returns int value
+     */
     public int compareTo(final Book obj) {
         return this.getName().compareTo(obj.getName());
     }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return this.name + ", " + this.author + ", "
                + this.price;
     }
 }
-
+/**.
+ * Class for binary search tree.
+ *
+ * @param      <Key>     Book object with book details
+ * @param      <Values>  The values
+ */
 class BinarySearchT<Key extends Comparable<Key>, Values> {
-
+    /**.
+     * starting element in the BST.
+     */
     private Node root;
-
+    /**.
+     * Class for node.
+     */
     class Node {
-
+        /**.
+         * Object of Book class with book details.
+         */
         private Key key;
-
+        /**.
+         * value of the book in Book class
+         */
         private Values value;
-
+        /**.
+         * Right node of the BST
+         */
         private Node right;
-
+        /**.
+         * Left node of BST.
+         */
         private Node left;
-
+        /**
+         * size of the tree at that node.
+         */
         private int size;
-
+        /**
+         * Constructs the object.
+         *
+         * @param      key1   The key 1
+         * @param      val    The value
+         * @param      size1  The size 1
+         */
         Node(final Key key1, final Values val, final int size1) {
             this.key = key1;
             this.value = val;
@@ -51,14 +107,32 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
         }
 
     }
-
+    /**.
+     * This method is to add a key and value to BST.
+     * Time Complexity is O(N).
+     * because it calls another put method to add elements.
+     *
+     * @param      key    The key
+     * @param      value  The value
+     */
     public void put(final Key key, final Values value) {
         if (key == null) {
             return;
         }
         root = put(root, key, value);
     }
-
+    /**.
+     * This method is to add element to BST
+     * Time Complexity is O(N) for worst case.
+     * element is added until it reaches to the
+     * position it should added at.
+     *
+     * @param      node   The node
+     * @param      key    The key
+     * @param      value  The value
+     *
+     * @return     returns node
+     */
     public Node put(final Node node, final Key key,
                     final Values value) {
         if (node == null) {
@@ -75,11 +149,27 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
         node.size = 1 + size(node.left) + size(node.right);
         return node;
     }
-
+    /**.
+     * This method is to return the value of that key
+     * Time Complexity is O(N)
+     *
+     * @param      key   The key
+     *
+     * @return     returns the value.
+     */
     public Values get(final Key key) {
         return get(root, key);
     }
-
+    /**.
+     * This method is to return the value of that key
+     * Time Complexity is O(N)
+     *
+     * @param      node  the node where the book details and
+     *                   values.
+     * @param      key   The key
+     *
+     * @return     returns the value of that key.
+     */
     private Values get(final Node node, final Key key) {
         if (node == null) {
             return null;
@@ -93,11 +183,25 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return node.value;
         }
     }
-
+    /**.
+     * this method returns minimum element in the tree
+     * Time complexity is O(logN)
+     *
+     * @return     returns minimum book details in the tree
+     */
     public Key min() {
         return min(root).key;
     }
-
+    /**.
+     * this method returns minimum element in the tree
+     * Time complexity is O(logN)
+     * it checks only left side of tree to find minimum
+     * element.
+     *
+     * @param      node     node it starts the checkong
+     *
+     * @return     returns minimum book details in the tree
+     */
     private Node min(final Node node) {
         if (node.left == null) {
             return node;
@@ -105,11 +209,24 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return min(node.left);
         }
     }
-
+    /**.
+     * this method returns maximum element in the tree
+     * Time complexity is O(logN)
+     *
+     * @return     returns maximum element in the tree
+     */
     public Key max() {
         return max(root).key;
     }
-
+    /**.
+     * this method returns maximum element in the tree
+     * Time complexity is O(logN)
+     * it checks only right side of tree to find max element
+     *
+     * @param      node  The node
+     *
+     * @return     eturns maximum element in the tree
+     */
     private Node max(final Node node) {
         if (node.right == null) {
             return node;
@@ -117,7 +234,16 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return max(node.right);
         }
     }
-
+    /**.
+     * this method returns the value of the given
+     * book data else returns null.
+     * Time complexity is O(logN)
+     *
+     * @param      key   The key
+     *
+     * @return     this method returns the value of the given
+     *             book data else returns null.
+     */
     public Key floor(final Key key) {
         Node node = floor(root, key);
         if (node == null) {
@@ -126,7 +252,18 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return node.key;
         }
     }
-
+    /**
+     * this method returns the value of the given
+     * book data else returns null.
+     * Time complexity is O(logN)
+     * it checks the element from root node to desired node
+     *
+     * @param      node     node with bookdetails
+     * @param      key   The key
+     *
+     * @return     this method returns the value of the given
+     *             book data else returns null.
+     */
     private Node floor(final Node node, final Key key) {
         if (node == null) {
             return null;
@@ -145,7 +282,16 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return node;
         }
     }
-
+    /**.
+     * this method returns the value of the given
+     * book data else returns null.
+     * Time complexity is O(logN)
+     *
+     * @param      key   The key
+     *
+     * @return     this method returns the value of the given
+     *             book data else returns null.
+     */
     public Key ceiling(final Key key) {
         Node node = ceiling(root, key);
         if (node == null) {
@@ -154,7 +300,18 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return node.key;
         }
     }
-
+    /**.
+     * this method returns the value of the given
+     * book data else returns null.
+     * Time complexity is O(logN)
+     * it checks the element from root node to desired node
+     *
+     * @param      node  The node
+     * @param      key   The key
+     *
+     * @return     this method returns the value of the given
+     *             book data else returns null.
+     */
     private Node ceiling(final Node node, final Key key) {
         if (node == null) {
             return null;
@@ -173,16 +330,35 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
         }
         return ceiling(node.right, key);
     }
-
+    /**.
+     * this method returns the element in that position
+     * Time complexity is O(logN)
+     *
+     * @param      k     position given as input
+     *
+     * @return     returns the element in that position
+     */
     public Key select(final int k) {
         Node node = select(root, k);
         return node.key;
     }
-
+    /**.
+     * size of tree
+     *
+     * @return     returns size of tree
+     */
     public int size() {
         return size(root);
     }
-
+    /**
+     * returns size of tree
+     * Time Complexity is O(1).
+     * it returns the size of that node.
+     *
+     * @param      node  The node
+     *
+     * @return     returns size of tree.
+     */
     private int size(final Node node) {
         if (node == null) {
             return 0;
@@ -190,7 +366,16 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return node.size;
         }
     }
-
+    /**.
+     * this method returns the element in that position
+     * Time complexity is O(logN)
+     * it checks from root node to desired position
+     *
+     * @param      node  The node
+     * @param      k     position of node
+     *
+     * @return     returns node of the that position
+     */
     private Node select(final Node node, final int k) {
         if (node == null) {
             return null;
@@ -204,11 +389,24 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
             return node;
         }
     }
-
+    /**.
+     * this method deletes maximum element from the tree.
+     * Time complexity is O(h)
+     * h is the height of the tree.
+     *
+     */
     public void deleteMax() {
         root = deleteMax(root);
     }
-
+    /**.
+     * this method deletes maximum element from the tree.
+     * Time complexity is O(h)
+     * h is the height of the tree.
+     *
+     * @param      node     the node
+     *
+     * @return     returns the node
+     */
     private Node deleteMax(final Node node) {
         if (node.right == null) {
             return node.left;
@@ -217,11 +415,23 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
         node.size = size(node.left) + size(node.right) + 1;
         return node;
     }
-
+    /**.
+     * this method deletes minimum element from the tree.
+     * Time complexity is O(h)
+     * h is the height of the tree.
+     */
     public void deleteMin() {
         root = deleteMin(root);
     }
-
+    /**.
+     * this method deletes minimum element from the tree.
+     * Time complexity is O(h)
+     * h is the height of the tree
+     *
+     * @param      node  The node
+     *
+     * @return     returns the node
+     */
     private Node deleteMin(final Node node) {
         if (node.left == null) {
             return node.right;
@@ -230,11 +440,26 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
         node.size = size(node.left) + size(node.right) + 1;
         return node;
     }
-
+    /**.
+     * this method deletes desired element from the tree.
+     * Time complexity is O(h)
+     * h is the height of the tree
+     *
+     * @param      key   The key
+     */
     public void delete(final Key key) {
         root = delete(root, key);
     }
-
+    /**.
+     * this method deletes desired element from the tree.
+     * Time complexity is O(h)
+     * h is the height of the tree
+     *
+     * @param      node  The node
+     * @param      key   The key
+     *
+     * @return     returns the node
+     */
     private Node delete(final Node node, final Key key) {
         Node temp = node;
         if (temp == null) {
@@ -263,12 +488,21 @@ class BinarySearchT<Key extends Comparable<Key>, Values> {
     }
 
 }
-
+/**
+ * Class for solution.
+ */
 final class Solution {
-
+    /**.
+     * Constructs the object.
+     */
     private Solution() {
 
     }
+    /**.
+     * main method to perform operations
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         BinarySearchT<Book, Integer> obj = new BinarySearchT<>();
